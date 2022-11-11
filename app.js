bb6336ee9a57199c7a2e8d82b19741327d27058f
Rebeccacheptoek <rebbytoek095@gmail.com>
2022-11-11 13:56:06 +0300

    basic js game

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-'use strict';
-const switcher = document.querySelector('.btn');
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
-    const className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark";
-    } else {
-        this.textContent = "Light";
-    }
-    console.log('current class name: ' + className);
+// 'use strict';
+// const switcher = document.querySelector('.btn');
+// switcher.addEventListener('click', function() {
+//     document.body.classList.toggle('light-theme');
+//     document.body.classList.toggle('dark-theme');
+//     const className = document.body.className;
+//     if(className == "light-theme") {
+//         this.textContent = "Dark";
+//     } else {
+//         this.textContent = "Light";
+//     }
+//     console.log('current class name: ' + className);
 
-});
+// });
+
+const STARTING_POKER_CHIPS = 100; // points
+const PLAYERS = 3;
+const NO_OF_STARTER_CARDS = 2;
+let gameHasEnded = false;
+
+let playerOneName = "Chloe";
+let playerTwoName = "Jasmine";
+let playerThreeName = "Jen";
+
+console.log(`Welcome to Texas Hold'em. The championship title will be awarded to one of these three players: ${playerOneName}, ${playerTwoName}, and ${playerThreeName}. Each player has ${STARTING_POKER_CHIPS} in their pot. We have an exciting game ahead of us. May the best player win!`);
+
+let playerOnePoints = STARTING_POKER_CHIPS;
+let playerTwoPoints = STARTING_POKER_CHIPS;
+let playerThreePoints = STARTING_POKER_CHIPS;
+
+playerOnePoints -= 50;
+playerTwoPoints -=25;
+playerThreePoints +=75; 
+
+gameHasEnded = ((playerOnePoints + playerTwoPoints) == 0) || // three has one
+               ((playerTwoPoints + playerThreePoints) == 0) ||  // one has won
+               ((playerOnePoints + playerThreePoints) == 0);  // two has won 
+
+console.log("Game has ended: ", gameHasEnded);
